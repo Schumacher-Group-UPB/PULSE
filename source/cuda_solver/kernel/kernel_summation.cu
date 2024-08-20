b2b7de302d0a53a7c91249e1166124aee0abd384
David Bauch <davidbauch12@gmail.com>
2024-08-20 14:43:10 +0200

    Fixed missing 1/dt in summation kernel

--- source/cuda_solver/kernel/kernel_summation.cu
+++ source/cuda_solver/kernel/kernel_summation.cu
@@ -24,7 +24,7 @@ PULSE_GLOBAL void PC3::Kernel::RK::runge_sum_to_input_kw( int i, Solver::KernelA
     Type::complex rv = 0.0;
     Type::complex dw = 0.0;
     if (args.p.stochastic_amplitude > 0.0) {
-        dw = args.dev_ptrs.random_number[i] * CUDA::sqrt( ( args.p.gamma_c ) / (Type::real(4.0) * args.p.dV) );
+        dw = args.dev_ptrs.random_number[i] * CUDA::sqrt( ( args.p.R * args.dev_ptrs.reservoir_plus[i] + args.p.gamma_c ) / (Type::real(4.0) * args.p.dV) );
     }
     for (int n = weights.start; n < weights.n; n++) {
         const Type::real w = weights.weights[n];
