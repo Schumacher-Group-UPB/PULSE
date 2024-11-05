3c7ec43b905e383d91693e5712e9aca83603bb87
Robert Schade <robert.schade@uni-paderborn.de>
2024-11-05 18:30:37 +0100

    csv exporter

---
+++ bench/README.md
@@ -0,0 +1,109 @@
+# Benchmarking
+
+## Setting up a Benchmark Series
+### Configuration
+A series of benchmarks is defined by a json file like
+```
+{
+  "commits":[
+    {"label":"bench","repo":"https://github.com/davidbauch/PHOENIX/","type":"branch"},
+    {"label":"4fa2c3b","repo":"https://github.com/davidbauch/PHOENIX/","type":"commit"},
+  ],
+  "envs":
+  [
+    {"label":"gcc-13.2.0","modules":["system/CUDA/12.6.0","tools/likwid/5.3.0-GCC-13.2.0","toolchain/foss/2023b","lang/Python/3.12.3-GCCcore-13.2.0"],"compiler":"g++","likwid":"likwid-perfctr","machinestate":"~/.local/bin/machinestate","nvidia-smi":"nvidia-smi","perf":"perf"}
+  ],
+  "makes":
+  [
+    {"label":"full","cmd":"make SFML=FALSE FP32=TRUE TETM=FALSE CPU=TRUE COMPILER=_compiler_ NO_CALCULATE_K=FALSE NO_INTERMEDIATE_SUM_K=FALSE NO_FINAL_SUM_K=FALSE NO_HALO_SYNC=FALSE AVX2=TRUE BENCH=TRUE LIKWID=TRUE"},                                   
+    {"label":"halo_sync","cmd":"make SFML=FALSE FP32=TRUE TETM=FALSE CPU=TRUE COMPILER=_compiler_ NO_CALCULATE_K=TRUE NO_INTERMEDIATE_SUM_K=TRUE NO_FINAL_SUM_K=TRUE NO_HALO_SYNC=FALSE AVX2=TRUE BENCH=TRUE LIKWID=TRUE"}                                  
+  ],
+  "threads": [64],
+  "grids": [1024,2048],
+  "subgrids": [[8,8],[16,16]],
+  "likwid_metrics": ["ENERGY","FLOPS_SP"]
+}
+```
+
+The set of benchmarks cases to be run is the product of all lists of fields. 
+
+That means that in the above config in total 32 variants are run:
+
+* 2 versions of the code (branch bench and the commit 4fa2c3b)
+* 1 choice of the environemnt in terms of compilers
+* 2 variants of the code, one `full` and one that only contains the halo synchronization
+* 1 nums of threads
+* 2 choices of grid sizes (1024 and 2048)
+* 2 choices of subgrids (8x8 and 16x16)
+* 2 choices of performance metrics to be recorded (`ENERGY` and `FLOPS_SP`)
+
+To run these versions in total 4 variants of the solver have to be compiled:
+* 2 versions of the code (branch bench and the commit 4fa2c3b)
+* 1 choice of the environemnt in terms of compilers
+* 2 variants of the code, one `full` and one that only contains the halo synchronization
+
+### Collecting Additional Information
+During a benchmark run additional information can be collected with tools. This is configured via the `envs` field. To deactive a tool, remove the mention from the `envs` field. The entry in the `envs`-fields are the commands to run the tools.
+
+#### Machinestate
+Machinestate (https://github.com/RRZE-HPC/MachineState) is a nice tool to automatically document the current system settings and the execution environment in terms of software and hardware for a benchmark run. The result is a json file `machinestate.json` in the subdirectory of the benchmark run.
+
+#### LIKWID
+LIKWID (https://github.com/RRZE-HPC/likwid) is a nice tool to access hardware-performance counter during a benchmark run to a measure for example the flaoting point throughput or cache utilization. The result is a json file `likwid.json` in the subdirectory of the benchmark run. Due to limitations of the number of registers for the hardwrae performance counter not all metrics can be measured at the same time. Thus, the full set is measured with multiple benchmark runs instead. Important metrics are directly included in the main result file `run.json` in the subdirectory of the benchmark run. (`cpu_energy_J_likwid` only has a valid value if in this run the `ENERGY` metric is measured with LIKWID.)
+
+#### Linux Perf
+In additiog to LIKWID (or if LIKWID is not available) the energy consumption of the CPU sockets can be measured with the Linux perf interface via RAPL energy counter. The result file `perf.out` contains the energy consumption of the CPU sockets during the calculation. The energy usage is included in the main result file `run.json` as `cpu_energy_J_perf` in the subdirectory of the benchmark run.
+
+#### nvidia-smi
+nvidia-smi can be used to measure, for exmaple, the power usage of NVIDIA GPUs. The result file `nvidia.csv` contains the energy consumption of the GPU sockets during the calculation. The energy usage is included in the main result file `run.json` as `gpu_energy` in the subdirectory of the benchmark run.
+
+## Running a Benchmark
+The benchmark script `bench.py` will perform the steps (compiling, running and summarizing) automatically. It will create the directories:
+* `runs/[name of the json config file]/0`, `runs/[name of the json config file]/1`,... for the benchmark runs defined by the json config
+* `bins/build_[commit label]_[env label]_[make label]`,... for the binaries used for the benchmark runs
+
+It should be run as `python bench.py -c [json config file]`.
+
+For each benchmark run, the main result file `run.json` holds all relevant results
+```
+{
+    "bindir": "/scratch/pc2-mitarbeiter/rschade/tickets/pulse/PHOENIX/bench/build_bench_gcc-13.2.0_full", 
+    "wall": 10.0, 
+    "ms/ps": 0.261, 
+    "ps/s": 3830.0, 
+    "mus/it": 35.8, 
+    "gpu_energy_J": 0, 
+    "cpu_energy_J_perf": 2611.95, 
+    "cpu_energy_J_likwid": 0, 
+    "likwid_measurement": 296446.8005, 
+    "commits": {"label": "bench", "repo": "https://github.com/davidbauch/PHOENIX/", "type": "branch"}, 
+    "envs": {
+        "label": "gcc-13.2.0", 
+        "modules": ["system/CUDA/12.6.0", "tools/likwid/5.3.0-GCC-13.2.0", "toolchain/foss/2023b", "lang/Python/3.12.3-GCCcore-13.2.0"], 
+        "compiler": "g++", 
+        "likwid": "likwid-perfctr", 
+        "machinestate": "~/.local/bin/machinestate", 
+        "nvidia-smi": "nvidia-smi", 
+        "perf": "perf"
+    }, 
+    "makes": {
+        "label": "full", 
+        "cmd": "make SFML=FALSE FP32=TRUE TETM=FALSE CPU=TRUE COMPILER=_compiler_ NO_CALCULATE_K=FALSE NO_INTERMEDIATE_SUM_K=FALSE NO_FINAL_SUM_K=FALSE NO_HALO_SYNC=FALSE AVX2=TRUE BENCH=TRUE LIKWID=TRUE"
+    }, 
+    "threads": 64, 
+    "grids": 192, 
+    "subgrids": [8, 8], 
+    "likwid_metrics": "FLOPS_SP"
+}
+```
+
+## Creating CSV-Files and Plotting
+A csv file can be produced from the benchmark runs for example with
+```
+python3 bench.py -c noctua2_7763.json --dryrun --fields=grids,subgrids,mus/it,likwid_metrics,likwid_measurement,threads=64 --output grids_7763.csv
+```
+The meaning of the command line arguments is:
+
+* `--dryrun` or `-d`: doesn't build and run the benchmarks but just uses the data availbale in the directory structure
+* `--output` or `-o`: path of the csv output file
+* `--fields` or `-f`: specifies the fields to be included in the csv. Filters can be defined with `fieldname=value`
--- bench/bench.py
+++ bench/bench.py
@@ -7,11 +7,10 @@ import subprocess
 def gv(data,c,s):
     i=0
     for f in data:
-        if f!="name":
-            if f==s:
-                break
-            else:
-                i=i+1
+        if f==s:
+            break
+        else:
+            i=i+1
     return c[i]
 
 def runcmd(cmd):
@@ -21,151 +20,215 @@ def runcmd(cmd):
     return output
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", help="increase output verbosity",action="store_true")
 parser.add_argument("-c","--config", help="json config for benchmask")
+parser.add_argument("-o","--output", help="output csv file",default="result.csv")
+parser.add_argument("-f","--fields", help="fields to include in output",default="grids,subgrids,threads")
+parser.add_argument("-d","--dryrun", help="only write output file from existing data, don't build and run benchmarks",action='store_true')
 args=parser.parse_args()
 
 with open(args.config, 'r') as file:
     data = json.load(file)
 
+name=args.config.replace(".json","")
+
 #build all combinations
 a=[]
 for f in data:
-    if f!="name":
-        a.append(data[f])
-print(a)
+    a.append(data[f])
 comb=list(itertools.product(*a))
 print("runs=",len(comb))
 
-os.makedirs(os.path.join("runs",data["name"]),exist_ok=True)
-rebuild=["commits","envs","makes"]
-last={}
-for ic in range(len(comb)):
-    #build run
-    d=os.path.join("runs",data["name"],str(ic))
-    os.makedirs(d,exist_ok=True)
+if not args.dryrun:
+    os.makedirs(os.path.join("runs",name),exist_ok=True)
+    rebuild=["commits","envs","makes"]
+    last={}
+    for ic in range(len(comb)):
+        #build run
+        d=os.path.join("runs",name,str(ic))
+        os.makedirs(d,exist_ok=True)
 
-    #build code
-    bindir="build"
-    for i in rebuild:
-        print(i,gv(data,comb[ic],i))
-        bindir+="_"+gv(data,comb[ic],i)["label"]
-    buildscript="#!/bin/bash\n"
-    buildscript+="mkdir bins\n"
-    buildscript+="cd bins\n"
-    buildscript+="git clone "+gv(data,comb[ic],"commits")["repo"]+"\n"
-    buildscript+="rm -rf \""+bindir+"\"\n"
-    buildscript+="mv PHOENIX \""+bindir+"\"\n"
-    buildscript+="cd \""+bindir+"\"\n"
-    if gv(data,comb[ic],"commits")["type"]=="branch":
-        buildscript+="git branch "+gv(data,comb[ic],"commits")["label"]+" origin/"+gv(data,comb[ic],"commits")["label"]+"\n"
-        buildscript+="git checkout "+gv(data,comb[ic],"commits")["label"]+"\n"
-    if gv(data,comb[ic],"commits")["type"]=="commit":
-        buildscript+="git checkout "+gv(data,comb[ic],"commits")["label"]+"\n"
-    env=gv(data,comb[ic],"envs")
-    envlabel=env["label"]
-    if len(env["modules"])>0:
-        buildscript+="module reset\n"
-        for m in env["modules"]:
-            buildscript+="module load "+m+"\n"
-    buildscript+="make clean\n"
-    make=gv(data,comb[ic],"makes")
-    makecmd=make["cmd"]
-    #replace setting from env in make command
-    for i in env:
-        if isinstance(env[i],str):
-            makecmd=makecmd.replace("_"+i+"_",env[i])
-    makelabel=make["label"]
-    buildscript+=makecmd+"\n"
-    with open(os.path.join(d,'build.sh'), 'w') as f:
-        f.write(buildscript)
-
-    #build version if necessary
-    dorebuild=False
-    if len(last)==0:
+        #build code
+        bindir="build"
         for i in rebuild:
-            last[i]=gv(data,comb[ic],i)
-        dorebuild=True
-    else:
-        for i in rebuild:
-             if last[i]!=gv(data,comb[ic],i):
-                dorebuild=True
-    if dorebuild:
-        for i in rebuild:
-            last[i]=gv(data,comb[ic],i)
-        print("building ",last)
-        runcmd("bash "+os.path.join(d,'build.sh'))
+            bindir+="_"+gv(data,comb[ic],i)["label"]
+        buildscript="#!/bin/bash\n"
+        buildscript+="mkdir bins\n"
+        buildscript+="cd bins\n"
+        buildscript+="git clone "+gv(data,comb[ic],"commits")["repo"]+"\n"
+        buildscript+="rm -rf \""+bindir+"\"\n"
+        buildscript+="mv PHOENIX \""+bindir+"\"\n"
+        buildscript+="cd \""+bindir+"\"\n"
+        if gv(data,comb[ic],"commits")["type"]=="branch":
+            buildscript+="git branch "+gv(data,comb[ic],"commits")["label"]+" origin/"+gv(data,comb[ic],"commits")["label"]+"\n"
+            buildscript+="git checkout "+gv(data,comb[ic],"commits")["label"]+"\n"
+        if gv(data,comb[ic],"commits")["type"]=="commit":
+            buildscript+="git checkout "+gv(data,comb[ic],"commits")["label"]+"\n"
+        env=gv(data,comb[ic],"envs")
+        envlabel=env["label"]
+        if len(env["modules"])>0:
+            buildscript+="module reset\n"
+            for m in env["modules"]:
+                buildscript+="module load "+m+"\n"
+        buildscript+="make clean\n"
+        make=gv(data,comb[ic],"makes")
+        makecmd=make["cmd"]
+        #replace setting from env in make command
+        for i in env:
+            if isinstance(env[i],str):
+                makecmd=makecmd.replace("_"+i+"_",env[i])
+        makelabel=make["label"]
+        buildscript+=makecmd+"\n"
+        with open(os.path.join(d,'build.sh'), 'w') as f:
+            f.write(buildscript)
 
-    #build run script
-    runscript="#!/bin/bash\n"
-    runscript+="cd \""+d+"\"\n"
-    runscript+="export OMP_PLACES=cores\n"
-    if len(env["modules"])>0:
-        runscript+="module reset\n"
-        for m in env["modules"]:
-            runscript+="module load "+m+"\n"
-    runscript+="export OMP_NUM_THREADS="+str(gv(data,comb[ic],"threads"))+"\n"
-    runscript+="export OMP_PROC_BIND=true\n"
-    runscript+="export OMP_PLACES=cores\n"
-    rcmd=""
-    if "machinestate" in env:
-        runscript+=env["machinestate"]+" > machinestate.json\n"
-    runscript+=env["compiler"]+" --version > compiler.version\n"
-    if "nvidia-smi" in env:
-        runscript+=env["nvidia-smi"]+" --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -lms 100 -i 0 > nvidia.csv & \n"
-        runscript+="nvpid=$!\n"
-    rcmd=""
-    if "perf" in env:
-        rcmd=env["perf"]+" stat -e power/energy-pkg/ --per-socket -x \",\" -o perf.out "+rcmd
-    if "likwid" in env:
-        rcmd=env["likwid"]+" -m -g "+gv(data,comb[ic],"likwid_metrics")+" -C 0-"+str(gv(data,comb[ic],"threads")-1)+" -o likwid.json "+rcmd
-    rcmd+="../../../bins/"+bindir+"/main.o -L 200 200 --tmax 1000 --gammaC 0.01 --gc 6E-6 --gr 12E-6 --initRandom 0.01 4242 100 --outEvery 1000 --threads "+str(gv(data,comb[ic],"threads"))+" --path output --N "+str(gv(data,comb[ic],"grids"))+" "+str(gv(data,comb[ic],"grids"))+" --output psi_plus --fftEvery 100000000 --boundary zero zero --subgrids "+str(gv(data,comb[ic],"subgrids")[0])+" "+str(gv(data,comb[ic],"subgrids")[1])+" > out 2> err"
-    runscript+=rcmd+"\n"
-    if "nvidia-smi" in env:
-        runscript+="kill $nvpid\n"
-    with open(os.path.join(d,'run.sh'), 'w') as f:
-        f.write(runscript)
-    runcmd("bash "+os.path.join(d,'run.sh'))
+        #build version if necessary
+        dorebuild=False
+        if len(last)==0:
+            for i in rebuild:
+                last[i]=gv(data,comb[ic],i)
+            dorebuild=True
+        else:
+            for i in rebuild:
+                 if last[i]!=gv(data,comb[ic],i):
+                    dorebuild=True
+        if dorebuild:
+            for i in rebuild:
+                last[i]=gv(data,comb[ic],i)
+            print("building ",last)
+            runcmd("bash "+os.path.join(d,'build.sh'))
 
-    run={"bindir":os.path.join(os.getcwd(),bindir)}
-    #parse results
-    #Total Runtime: 10s --> 0.26ms/ps --> 3.84e+03ps/s --> 35.6 mus/it
-    with open(os.path.join(d,"out")) as f:
-        for l in f.read().splitlines():
-            if l.startswith("Total Runtime: "):
-                run["wall"]=float(l.split()[2].replace("s",""))
-                run["ms/ps"]=float(l.split()[4].replace("ms/ps",""))
-                run["ps/s"]=float(l.split()[6].replace("ps/s",""))
-                run["mus/it"]=float(l.split()[8].replace("mus/it",""))
-                break
-    
-    #get power usage
-    #cpu
-    if gv(data,comb[ic],"likwid_metrics")=="ENERGY":
-        with open(os.path.join(d,"likwid.json")) as f:
-            likwid_data = json.load(f)
-        Pcpu=likwid_data["Energy"]["Energy"]["Metric STAT"]["Power PKG [W] STAT"]
-   
-    #gpu
-    Pgpu=-1
-    if "nvidia-smi" in env:
-        with open(os.path.join(d,"nvidia.csv")) as f:
+        #build run script
+        runscript="#!/bin/bash\n"
+        runscript+="cd \""+d+"\"\n"
+        runscript+="export OMP_PLACES=cores\n"
+        if len(env["modules"])>0:
+            runscript+="module reset\n"
+            for m in env["modules"]:
+                runscript+="module load "+m+"\n"
+        runscript+="export OMP_NUM_THREADS="+str(gv(data,comb[ic],"threads"))+"\n"
+        runscript+="export OMP_PROC_BIND=true\n"
+        runscript+="export OMP_PLACES=cores\n"
+        rcmd=""
+        if "machinestate" in env:
+            runscript+=env["machinestate"]+" > machinestate.json\n"
+        runscript+=env["compiler"]+" --version > compiler.version\n"
+        if "nvidia-smi" in env:
+            runscript+=env["nvidia-smi"]+" --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -lms 100 -i 0 > nvidia.csv & \n"
+            runscript+="nvpid=$!\n"
+        rcmd=""
+        if "perf" in env:
+            rcmd=env["perf"]+" stat -e power/energy-pkg/ --per-socket -x \";\" -o perf.out "+rcmd
+        if "likwid" in env:
+            rcmd=env["likwid"]+" -m -g "+gv(data,comb[ic],"likwid_metrics")+" -C 0-"+str(gv(data,comb[ic],"threads")-1)+" -o likwid.json "+rcmd
+        rcmd+="../../../bins/"+bindir+"/main.o -L 200 200 --tmax 1000 --gammaC 0.01 --gc 6E-6 --gr 12E-6 --initRandom 0.01 4242 100 --outEvery 1000 --threads "+str(gv(data,comb[ic],"threads"))+" --path output --N "+str(gv(data,comb[ic],"grids"))+" "+str(gv(data,comb[ic],"grids"))+" --output psi_plus --fftEvery 100000000 --boundary zero zero --subgrids "+str(gv(data,comb[ic],"subgrids")[0])+" "+str(gv(data,comb[ic],"subgrids")[1])+" > out 2> err"
+        runscript+=rcmd+"\n"
+        if "nvidia-smi" in env:
+            runscript+="kill $nvpid\n"
+        with open(os.path.join(d,'run.sh'), 'w') as f:
+            f.write(runscript)
+        runcmd("bash "+os.path.join(d,'run.sh'))
+
+        #analyse benchmark and write result file
+        
+        run={"bindir":os.path.join(os.getcwd(),bindir)}
+        #parse results
+        #Total Runtime: 10s --> 0.26ms/ps --> 3.84e+03ps/s --> 35.6 mus/it
+        with open(os.path.join(d,"out")) as f:
             for l in f.read().splitlines():
-                Pgpu=flaot(l.split(",")[2].split(" ")[1])
-           
-    if Pgpu!=-1:
-        run["gpu_power_W"]=Pgpu
-    if Pcpu!=-1:
-        run["cpu_power_W"]=Pcpu
+                if l.startswith("Total Runtime: "):
+                    run["wall"]=float(l.split()[2].replace("s",""))
+                    run["ms/ps"]=float(l.split()[4].replace("ms/ps",""))
+                    run["ps/s"]=float(l.split()[6].replace("ps/s",""))
+                    run["mus/it"]=float(l.split()[8].replace("mus/it",""))
+                    break
+        
+        #map likwid jsons fileds to result fields
+        lmap={"ENERGY":["Energy PKG [J] STAT","Sum"],"DATA":["Load to store ratio STAT","avg"],"FLOPS_SP":["SP [MFLOP/s] STAT","Sum"],"CACHE":["data cache miss ratio STAT","Avg"],"L2":["L2 bandwidth [MBytes/s] STAT","Sum"],"L2CACHE":["L2 miss ratio STAT","Avg"],"L3":["L3 bandwidth [MBytes/s] STAT","Sum"],"MEM1":["Memory bandwidth (channels 0-3) [MBytes/s] STAT","sum"],"MEM2":["Memory bandwidth (channels 4-7) [MBytes/s] STAT","Sum"]}
+        #get power usage
+        #cpu
+        Ecpu=0
+        if "perf" in env:
+            with open(os.path.join(d,"perf.out")) as f:
+                for l in f.read().splitlines():
+                    if l.startswith("S0"):
+                        Ecpu=float(l.split(";")[2])
+                        break
+        Ecpu2=0
+        if "likwid" in env:
+            if gv(data,comb[ic],"likwid_metrics")=="ENERGY":
+                with open(os.path.join(d,"likwid.json")) as f:
+                    likwid_data = json.load(f)
+                Ecpu2=likwid_data["ENERGY"]["ENERGY"]["Metric STAT"][lmap["ENERGY"][0]][lmap["ENERGY"][1]]
+       
+        #gpu
+        Egpu=0
+        if "nvidia-smi" in env:
+            with open(os.path.join(d,"nvidia.csv")) as f:
+                for l in f.read().splitlines():
+                    Egpu+=float(l.split(",")[2].split(" ")[1])*0.1
+               
+        if Egpu!=-1:
+            run["gpu_energy_J"]=Egpu
+        if Ecpu!=-1:
+            run["cpu_energy_J_perf"]=Ecpu
+        if Ecpu2!=-1:
+            run["cpu_energy_J_likwid"]=Ecpu2
 
 
+        Mlikwid=0
+        if "likwid" in env:
+            try:
+                if gv(data,comb[ic],"likwid_metrics")!="":
+                    with open(os.path.join(d,"likwid.json")) as f:
+                        likwid_data = json.load(f)
+                    Mlikwid=likwid_data[gv(data,comb[ic],"likwid_metrics")][gv(data,comb[ic],"likwid_metrics")]["Metric STAT"][lmap[gv(data,comb[ic],"likwid_metrics")][0]][lmap[gv(data,comb[ic],"likwid_metrics")][1]]
+            except:
+                pass
+        run["likwid_measurement"]=Mlikwid
 
-    #write json description file
-    for f in data:
-        if f!="name":
+
+        #write json description file
+        for f in data:
             run[f]=gv(data,comb[ic],f)
-    with open(os.path.join(d,'run.json'), 'w') as f:
-            json.dump(run, f)
+        with open(os.path.join(d,'run.json'), 'w') as f:
+                json.dump(run, f)
+
+#build csv output file
+print("fields for output:",args.fields)
+res="#"
+for f in args.fields.split(","):
+    if not f.find("=")>=0:
+        res+=f+";"
+res+="\n"
+
+for ic in range(len(comb)):
+    d=os.path.join("runs",name,str(ic))
+    with open(os.path.join(d,"run.json"), 'r') as file:
+        data = json.load(file)
+    add=True
+    for f in args.fields.split(","):
+        #check if is filter
+        if f.find("=")>=0:
+            g=f.split("=")[0]
+            v=f.split("=")[1]
+            if str(data[g]).strip()!=str(v).strip():
+                add=False
+    #build line
+    if add:
+        for f in args.fields.split(","):
+            if not f.find("=")>=0:
+                s=str(data[f])
+                if isinstance(data[f],list):
+                    s=",".join(str(x) for x in data[f])
+                res+=s+";"
+        res+="\n"
+
+with open(args.output, 'w') as f:
+    f.write(res)
+    
+
+
+
 
 
 
