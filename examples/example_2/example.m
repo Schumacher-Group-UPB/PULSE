37c350b1c79471049d7e9aa75b7b6e930fb74b28
Jan Wingenbach <139117697+JanWI1@users.noreply.github.com>
2024-11-22 13:19:49 +0100

    example.m

--- examples/example_2/example.m
+++ examples/example_2/example.m
@@ -1,23 +1,20 @@
 clear;    
-%--------------------------------Misc---------------------------------%
 optimizer = 0;        %find the exceptional point using a optimizer algorythm
 
-%--------------------------------time evolution settings---------------------------------%
+% time evolution settings
 dt=0.02;
 tmax=1500;
-lambda_min=2;
-lambda_max=6;
 FFT_sample_rate=1;
 sample_t0=500;
 
-%----------------------------------system discretisation---------------------------------%
+% system discretisation
 N=200;  dxy=0.1;  L=N*dxy;
 x=sparse(dxy*(-N/2+1:N/2)).';
 y=sparse(dxy*(-N/2+1:N/2)).';
 kx=(2*pi*(-N/2+1:N/2)/L).';
 ky=(2*pi*(-N/2+1:N/2)/L).';
 
-%--------------------------------GP - parameter definition-------------------------------%
+% parameter definition
 hbar = 6.582119569E-1;  
 mc = 0.5*5.864E-1;      %effective polariton mass (0.5E-4*me)
 gammaC = 0.16;          %polariton loss rate
@@ -26,7 +23,7 @@ R = 0.01;               %condensation rate
 gc = 6E-3;              %polariton-polariton interaction strength
 gr = 2*gc;              %polariton-reservoir interaction strength      
 
-%----------------------------------shape external potential------------------------------%
+% create potential and set pump parameters
 V=zeros(N,N);
 V1 = -2.2;              %potential depth 1
 v1w=1.5;                %potential width 1
@@ -38,6 +35,8 @@ dis=4;                  %potential well separation
 P1 =12;                 %pump intensity 1
 p1w=1.0;                %pump width 1
 p2w=p1w;                %pump width 2
+lambda_min=2;           %set lower bound for P2
+lambda_max=6;           %set upper bound for P2
 
 for i=1:N
     for j=1:N
@@ -48,7 +47,7 @@ end
 potential(1:N,1:N) = real(V);
 potential(N+1:2*N,1:N) = imag(V);
 
-%----- create 2D Hamiltonian with periodisc boundary conditions -----%
+% solve eigenvalue problem to determine initial condition 
 a=hbar^2/(2*mc)*4/(dxy)^2;
 b=hbar^2/(2*mc)*(-1/(dxy)^2);
 a1=sptoeplitz( [a b zeros(1,N-3) b],[a b zeros(1,N-3) b]);
@@ -58,7 +57,6 @@ H(1:N,N^2-N+1:N^2)=b1;
 H(N^2-N+1:N^2,1:N)=b1;
 M=H+speye(N^2,N^2).*reshape(V,N^2,1);
 
-%--- solve eigenvalue problem of H+V to generate intial condition ---%
 [Evec,Eval]=eigs(M,12,'sa','Tolerance',1e-6);
 psi=75*reshape(Evec(:,2),N,N);   n=zeros(N,N);
 
@@ -72,15 +70,10 @@ writecell(header,'data/load/potential_plus.txt','Delimiter',' ');
 writecell(header,'data/load/wavefunction_plus.txt','Delimiter',' ');
 writecell(header,'data/load/reservoir_plus.txt','Delimiter',' ');
 
-%writematrix(potential,'data/load/potential_plus.txt','Delimiter',' ','WriteMode','append');
 writematrix(wavefunction,'data/load/wavefunction_plus.txt','Delimiter',' ','WriteMode','append');
 writematrix(reservoir,'data/load/reservoir_plus.txt','Delimiter',' ','WriteMode','append');
 
-integrated_densities=zeros(lambda_max-lambda_min+1,1);
-pump_intensitys_scaned=zeros(lambda_max-lambda_min+1,1);
-E1 = zeros(lambda_max-lambda_min+1,1);
-E2 = zeros(lambda_max-lambda_min+1,1);
-
+% fprintf can be used to controll the number of digits to remain in the floating point limits
 fileID = fopen('data/load/potential_plus.txt', 'a'); % Open the file in append mode
 % Write the data with space delimiter
 for row = 1:size(potential, 1)
@@ -89,7 +82,13 @@ for row = 1:size(potential, 1)
 end
 fclose(fileID); % Close the file
 
+integrated_densities=zeros(lambda_max-lambda_min+1,1);
+pump_intensitys_scaned=zeros(lambda_max-lambda_min+1,1);
+E1 = zeros(lambda_max-lambda_min+1,1);
+E2 = zeros(lambda_max-lambda_min+1,1);
+
 for lambda=lambda_min:lambda_max
+    % create pump
     writecell(header,'data/load/pump_plus.txt','Delimiter',' ');
     P=zeros(N,N);
     P2 =lambda;  
@@ -104,7 +103,7 @@ for lambda=lambda_min:lambda_max
 
     writematrix(pump,'data/load/pump_plus.txt','Delimiter',' ','WriteMode','append')
 
-    %-----------------------------------call-PHOENIX-------------------------------------------%
+    % create runstring
     str1 = 'phoenix_gpu_fp32.exe ';                                                                                                 
     str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary periodic periodic '];
     str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftMask 1.0 add 0.2 0.2 0 0 both 6 none gauss+noDivide+local --fftEvery ',num2str(dt),' '];  
@@ -112,13 +111,15 @@ for lambda=lambda_min:lambda_max
     str5 = '--potential load data/load/potential_plus.txt 1 add both --initialState load data/load/wavefunction_plus.txt 1 add both --pump load data/load/pump_plus.txt 1 add both ';                                                                            
     str6 = ['--gammaC ',num2str(gammaC),' --gammaR ',num2str(gammaR),' --gc ',num2str(gc),' --gr ',num2str(gr),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' --R ',num2str(R),' '];                            
     str7 = '--path data/results/'; 
+    
+    % execute PHOENIX
     inputstr = [str1,str2,str3,str4,str5,str6,str7];
     [~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
 
     htmlString = removeAnsiCodes(cmdout);
     disp(htmlString(end-2630:end));    
-    %----------------------------------post-processing---------------------------------------%
-
+    
+    % post-processing: calculate mode spectrum
     psi = readmatrix('data/results/wavefunction_plus.txt');
     psi=psi(1:N,1:N)+1i*psi(N+1:2*N,1:N);
     Y1 = abs(reshape(psi,N,N)).^2;
@@ -159,6 +160,7 @@ for lambda=lambda_min:lambda_max
 end
 E2(E2==E1)=NaN;
 
+% post-processing: visualize results
 dim=[0.841333333333333,0.822916666666671,0.055333333333333,0.048333333333333];
 x0=10; y0=10; width=1500; height=1100;
 figure(3);
@@ -173,17 +175,18 @@ set(gcf,'position',get(0, 'Screensize'));box on;set(gca,'linewidth',2);
 set(gca,'XLim',[0 max(pump_intensitys_scaned)]);set(gca,'fontsize',24);
 set(gcf,'position',[x0,y0,width,height]);
 
-%----------------------------------Optimizer----------------------------------%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% this part of the code uses an local optimizer algorithm to localize the exceptional point
+
 options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1E-1);
 EP_loc = fminbnd(@costfunction,0,20,options);
 
-%----------------------------------String handeling functions----------------------------------%
 function cleanedString = removeAnsiCodes(inputString)
     % Use regexprep to remove ANSI escape codes
     cleanedString = strrep(inputString, '[90m#[0m', ' ');
     cleanedString = regexprep(cleanedString, '\[0m|\[1m|\[2m|\[3m|\[4m|\[30m|\[31m|\[32m|\[33m|\[34m|\[35m|\[36m|\[37m|[93m|\[94m|\[?25h|\[2K|\[?25l|\[90m|\[A', '');
 end
-%----------------------------------Matrix handeling functions----------------------------------%
+% matrix handeling functions
 function A = MatrixInsert(A,B,at)
 [w,h] = size(B);
 A(at(1):at(1)+w-1, at(2):at(2)+h-1) = B;
@@ -242,18 +245,21 @@ T = spdiags( B,d,m,n );
 end
 
 function cost = costfunction(X)
+
+% system discretisation
 N=200;                  %grid size
 dxy=0.1;                %spatial discretization step
 L=N*dxy;                %real space grid length
 x=sparse(dxy*(-N/2+1:N/2)).';   %x dimension discretization
 y=sparse(dxy*(-N/2+1:N/2)).';   %y dimension discretization
 
+% time evolution settings
 dt=0.02;                %time-evolution step
 tmax=2000;              %time-evolution range
 FFT_sample_rate=1;      %sample rate for wave-profile (affects the frequency range covered during the FFT)
-sample_t0=1500;          %off-set for FFT-sampling to ensure convergence of the system before the sampling
+sample_t0=1500;         %off-set for FFT-sampling to ensure convergence of the system before the sampling
 
-%-------------------------------GP - parameter definition-------------------------------%
+% parameter definition
 mc = 0.5*5.864E-4;      %effective polariton mass (0.5E-4*me)
 gammaC = 0.16;          %polariton loss rate
 gammaR = 1.5*gammaC;    %reservoir loss rate
@@ -268,7 +274,8 @@ p2w=p1w;                %pump width 2
 
 pump = writepump(X,N,L,dxy,x,y,dis,P1,p1w,p2w);
 delete('output\timeoutput\*.txt');
-%-----------------------------------call-PHOENIX-------------------------------------------%
+
+%create runstring
 str1 = 'phoenix_gpu_fp32.exe ';                                                                                                 
 str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary periodic periodic '];
 str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftMask 1.0 add 0.2 0.2 0 0 both 6 none gauss+noDivide+local --fftEvery ',num2str(dt),' '];  
@@ -276,9 +283,12 @@ str4 = ['--historyMatrix 0 ',num2str(N),' 0 ',num2str(N),' 1 --output wavefuncti
 str5 = '--potential load data/load/potential_plus.txt 1 add both --initialState load data/load/wavefunction_plus.txt 1 add both --pump load data/load/pump_plus.txt 1 add both ';                                                                            
 str6 = ['--gammaC ',num2str(gammaC),' --gammaR ',num2str(gammaR),' --gc ',num2str(gc),' --gr ',num2str(gr),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' --R ',num2str(R),' '];                            
 str7 = '--path data/results/'; 
+
+% execute PHOENIX
 inputstr = [str1,str2,str3,str4,str5,str6,str7];
 [~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
 
+%post-processing: localize exceptional point
 projectdir = 'output\timeoutput\';
 dinfo = dir( fullfile(projectdir, '*.txt'));
 nfiles = length(dinfo);
@@ -300,7 +310,8 @@ ax(2)=subplot(1,2,2);surf(x,y,angle(reshape(psi,N,N)));colorbar;shading interp;v
 pbaspect([1 1 1]);axis tight;set(gca,'YTickLabel',[]);xlabel('x (µm)');shading interp;set(gca, 'Fontsize',20);
 cost = sum(sum(y1sum));
 end
-%----------------------------------Pump-Adaptation Function for Optimizer----------------------------------%
+
+%post-processing: adapt the pump with the optimizer
 function P = writepump(lambda,N,L,dxy,x,y,dis,P1,p1w,p2w)
 header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) num2str(dxy)};
 writecell(header,'loads/pump_plus.txt','Delimiter',' ');
