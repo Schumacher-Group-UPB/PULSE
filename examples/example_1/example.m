0032a6999790bc56374c284d7ada10216a5a5904
Jan Wingenbach <139117697+JanWI1@users.noreply.github.com>
2024-11-22 13:09:21 +0100

    example.m

--- examples/example_1/example.m
+++ examples/example_1/example.m
@@ -1,31 +1,24 @@
-clear;    %clear Matlab Workspace
-rng('shuffle');
-delete('C:\Users\Jan Wingenbach\OneDrive - Universität Paderborn\Documents\GPU tests\example 1\PC3\load\*.txt');
-%------------------------------------------Misc------------------------------------------%
-gpudevice = 0;        %select your best GPU device (hint: 'gpuDeviceTable')
-
-%--------------------------------time evolution settings---------------------------------%
-tmax = 4200;              %time-evolution range
+clear;   
+% time evolution settings
+tmax = 5000;              %time-evolution range
 dt = 0.006;               %time-step
 
-%----------------------------------system discretisation---------------------------------%
+% system discretisation
 N = 600;                  %grid size
 dxy = 0.1;                 %spatial discretization step
 L = N*dxy;                 %real space grid length
 x = sparse(dxy*(-N/2+1:N/2)).';   %x dimension discretization
 y = sparse(dxy*(-N/2+1:N/2)).';   %y dimension discretization
-kx = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
-ky = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
 
-%--------------------------------GP - parameter definition-------------------------------%
+% parameter definition
 hbar = 6.582119569E-1;  %hbar in eVps^-1
 mc = 5.684E-1;          %effective polariton mass
 gammaC = 0.005;         %polariton loss rate in ps^-1
 gc = 2E-3;              %polariton-polariton interaction strength in eVum^2
 
+% create potential and initial condition
 load('potential_N600.mat'); pot=vpotmatrix;
 I_Noise = 1E-1; psi=I_Noise*(-1+2*rand(N,N)-1i+2*1i*rand(N,N)); 
-n=zeros(N,N);
 
 psi(1:N,1:N) = real(psi);
 psi(N+1:2*N,1:N) = imag(psi);
@@ -40,10 +33,11 @@ header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) nu
 writecell(header,'data/load/potential_plus.txt','Delimiter',' ');
 writematrix(potential,'data/load/potential_plus.txt','Delimiter',' ','WriteMode','append');
 
+% pulse parameters
 omega = -5.68828730684262;
 E0 = 5.6856E-3;
 
-%-----------------------------------call-pulse-------------------------------------------%
+%create runstring
 str1 = 'phoenix_64.exe ';                                                                                                  %call PULSE main.exe
 str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary zero zero '];                      %define the real-space grid
 str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftEvery 1000 '];                                 %time discretization
@@ -52,12 +46,13 @@ str5 = '--potential load data/load/potential_plus.txt 1 add plus --wavefunction 
 str6 = ['--gammaC ',num2str(gammaC),' --gc ',num2str(gc),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' '];                            %set GP-Parameters
 str7 = '--path data/results/';                                                                                                                  %set output directory
 
+% execute PHOENIX
 inputstr = [str1,str2,str3,str4,str5,str6,str7];
 [~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
 htmlString = removeAnsiCodes(cmdout);
 disp(htmlString(end-2630:end));
 
-%----------------------------------post-processing---------------------------------------%
+%post-processing: visualize results
 psi = readmatrix('data/results/wavefunction_plus.txt');
 psi=psi(1:N,1:N)+1i*psi(N+1:2*N,1:N);
 Y1 = abs(reshape(psi,N,N)).^2;
@@ -78,17 +73,18 @@ pbaspect([1 1 1]);axis tight;xlabel('x (µm)');ylabel('y (µm)');shading interp;
 ax(4)=subplot(2,2,4);surf(x,y,angle(reshape(psi,N,N)));colorbar;shading interp;view(2);colormap(ax(4),'jet');
 pbaspect([1 1 1]);axis tight;set(gca,'YTickLabel',[]);xlabel('x (µm)');shading interp;set(gca, 'Fontsize',20);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %this part of the code uses an local optimizer algorithm to optimize the resonance of the
 %pulse-frequency with  the corner state 
 options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1E-1);
 resonance = fminbnd(@costfunction,0,1,options);
 function cost = costfunction(X)
-%--------------------------------time evolution settings---------------------------------%
+
+% time evolution settings
 tmax = 4000;              %time-evolution range
 dt = 0.006;               %time-step
 
-%----------------------------------system discretisation---------------------------------%
+% system discretisation
 N = 600;                   %grid size
 dxy = 0.1;                 %spatial discretization step
 L = N*dxy;                 %real space grid length
@@ -97,13 +93,13 @@ y = sparse(dxy*(-N/2+1:N/2)).';   %y dimension discretization
 kx = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
 ky = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
 
-%--------------------------------GP - parameter definition-------------------------------%
+% parameter definition
 hbar = 6.582119569E-1;  %hbar in eVps^-1
 mc = 5.684E-1;          %effective polariton mass
 gammaC = 0.005;         %polariton loss rate in ps^-1
 gc = 2E-3;              %polariton-polariton interaction strength in eVum^2
-%--- write potential and initial condition into the load folder of PULSE ---%
 
+% create potential and initial condition
 load('potential_N600.mat'); pot=vpotmatrix;
 load('initialstate_600.mat','psi');
 
@@ -122,10 +118,11 @@ header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) nu
 writecell(header,'data/load/potential_plus.txt','Delimiter',' ');
 writematrix(potential,'data/load/potential_plus.txt','Delimiter',' ','WriteMode','append');
 
+% pulse parameters
 omega = -5.68178730684262-X*1e-2;
 E0 = 5.6856E-3;
-%-----------------------------------call-pulse-------------------------------------------%
 
+%create runstring
 str1 = 'phoenix_64.exe ';                                                                                                  %call PULSE main.exe
 str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary zero zero '];                      %define the real-space grid
 str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftEvery 1000 '];                                 %time discretization
@@ -134,12 +131,13 @@ str5 = '--potential load data/load/potential_plus.txt 1 add plus --wavefunction 
 str6 = ['--gammaC ',num2str(gammaC),' --gc ',num2str(gc),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' '];                            %set GP-Parameters
 str7 = '--path data/results/';                                                                                                                  %set output directory                                                                                     
 
+% execute PHOENIX
 inputstr = [str1,str2,str3,str4,str5,str6,str7];
 [~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
 htmlString = removeAnsiCodes(cmdout);
 disp(htmlString(end-2630:end));
 
-%----------------------------------post-processing---------------------------------------%
+%post-processing: visualize results and evaluate cost-function
 psi = readmatrix('data/results/wavefunction_plus.txt');
 psi=psi(1:N,1:N)+1i*psi(N+1:2*N,1:N);
 Y1 = abs(reshape(psi,N,N)).^2;
