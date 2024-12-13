fc901d88dddc4565dafa8bdf17c3e3bcc2d6a248
Jan Wingenbach <139117697+JanWI1@users.noreply.github.com>
2024-12-13 11:13:38 +0100

    Update example.m

--- examples/example_1/example.m
+++ examples/example_1/example.m
@@ -1,4 +1,50 @@
-clear;   
+clear; rng('shuffle');  
+%% Setting up the required directories and check for executable and GPU device (if required)
+% Define the filename of your PHOENIX release
+filename = 'phoenix_64.exe';
+
+% Specify a custom directory
+customDir = pwd;
+
+if exist(fullfile(customDir, filename), 'file') == 2
+    disp(['The file "', filename, '" is present in the directory.']);
+else
+    disp(['The file "', filename, '" is not found in the directory.']);
+end
+
+% Define loads directory path
+directoryPath_load = 'data/loads/';
+
+% Create loads directory
+if ~exist(directoryPath_load, 'dir')
+    % Create the directory
+    mkdir(directoryPath_load);
+    disp(['Directory "', directoryPath_load, '" has been created.']);
+end
+
+% Define result directory path
+directoryPath_result = 'data/results/';
+
+% Create results directory
+if ~exist(directoryPath_result, 'dir')
+    % Create the directory
+    mkdir(directoryPath_result);
+    disp(['Directory "', directoryPath_result, '" has been created.']);
+end
+
+% Get GPU device table
+gpuTable = gpuDeviceTable;
+
+% Check if the GPU device table is empty
+if isempty(gpuTable)
+    disp('No GPU device detected. Please use the CPU version of Phoenix.');
+else
+    disp('GPU device detected. Proceeding with GPU-based Phoenix.');
+end
+%% Set if you want to run the optimizer algorithm
+optimizer = 0;      %find the resoance energy to the corner state
+
+%% Setting up parameters and matrices
 % time evolution settings
 tmax = 5000;              %time-evolution range
 dt = 0.006;               %time-step
@@ -23,45 +69,53 @@ I_Noise = 1E-1; psi=I_Noise*(-1+2*rand(N,N)-1i+2*1i*rand(N,N));
 psi(1:N,1:N) = real(psi);
 psi(N+1:2*N,1:N) = imag(psi);
 header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) num2str(dxy)};
-writecell(header,'data/load/wavefunction_plus.txt','Delimiter',' ');
-writematrix(psi,'data/load/wavefunction_plus.txt','Delimiter',' ','WriteMode','append');
+writecell(header,[directoryPath_load,'wavefunction_plus.txt'],'Delimiter',' ');
+writematrix(psi,[directoryPath_load,'wavefunction_plus.txt'],'Delimiter',' ','WriteMode','append');
 
 potential(1:N,1:N) = real(pot*1e3);
 potential(N+1:2*N,1:N) = imag(pot);
-
-header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) num2str(dxy)};
-writecell(header,'data/load/potential_plus.txt','Delimiter',' ');
-writematrix(potential,'data/load/potential_plus.txt','Delimiter',' ','WriteMode','append');
+writecell(header,[directoryPath_load,'potential_plus.txt'],'Delimiter',' ');
+writematrix(potential,[directoryPath_load,'potential_plus.txt'],'Delimiter',' ','WriteMode','append');
 
 % pulse parameters
 omega = -5.68828730684262;
 E0 = 5.6856E-3;
 
-%create runstring
-str1 = 'phoenix_64.exe ';                                                                                                  %call PULSE main.exe
+%% Execute PHOENIX
+str1 = [filename, ' '];                                                                                                    %call main.exe
 str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary zero zero '];                      %define the real-space grid
 str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftEvery 1000 '];                                 %time discretization
 str4 = ['--pulse ',num2str(E0),' add 1E12 1E12 0 0 plus 10 0 gauss+noDivide time iexp 0 1E12 ',num2str(omega),' '];        %define resonant pump
-str5 = '--potential load data/load/potential_plus.txt 1 add plus --wavefunction load data/load/wavefunction_plus.txt 1 add plus ';              %load initial condition and potential
+str5 = ['--potential load ',directoryPath_load,'potential_plus.txt 1 add plus --wavefunction load ',directoryPath_load,'wavefunction_plus.txt 1 add plus '];      %load initial condition and potential
 str6 = ['--gammaC ',num2str(gammaC),' --gc ',num2str(gc),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' '];                            %set GP-Parameters
-str7 = '--path data/results/';                                                                                                                  %set output directory
+str7 = ['--path ',directoryPath_result];                                                                                                        %set output directory
 
 % execute PHOENIX
 inputstr = [str1,str2,str3,str4,str5,str6,str7];
-[~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
+[~,cmdout] = system(inputstr);
 
 % in cmdout you can view the output of PHOENIX
+phoenix_output = removeAnsiCodes(cmdout);
+disp(phoenix_output);
 
+% Check if PHOENIX was completed successfully
+if contains(phoenix_output, ' Runtime Statistics ')
+    disp('PHOENIX ran successfully.');
+else
+    disp('PHOENIX did not run successfully.');
+end
+
+%% Post-processing
 %post-processing: visualize results
-psi = readmatrix('data/results/wavefunction_plus.txt');
+psi = readmatrix([directoryPath_result,'wavefunction_plus.txt']);
 psi=psi(1:N,1:N)+1i*psi(N+1:2*N,1:N);
 Y1 = abs(reshape(psi,N,N)).^2;
 
-pump = readmatrix('data/results/pulse_plus.txt');
+pump = readmatrix([directoryPath_result,'pulse_plus.txt']);
 pump=pump(1:N,1:N)+1i*pump(N+1:2*N,1:N);
 
-pot = readmatrix('data/results/potential_plus.txt');
-pot=pot(1:N,1:N);%+1i*pot(N+1:2*N,1:N);
+pot = readmatrix([directoryPath_result,'potential_plus.txt']);
+pot=pot(1:N,1:N);
 
 figure(1);
 ax(1)=subplot(2,2,1);surf(x,y,pot);shading interp;view(2);colorbar;colormap(ax(1),'jet');
@@ -74,14 +128,23 @@ ax(4)=subplot(2,2,4);surf(x,y,angle(reshape(psi,N,N)));colorbar;shading interp;v
 pbaspect([1 1 1]);axis tight;set(gca,'YTickLabel',[]);xlabel('x (µm)');shading interp;set(gca, 'Fontsize',20);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%this part of the code uses an local optimizer algorithm to optimize the resonance of the
-%pulse-frequency with  the corner state 
-options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1E-1);
-resonance = fminbnd(@costfunction,0,1,options);
+%% Optimizer
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%this part of the code uses an local optimizer algorithm to optimize the resonance of the pulse-frequency with  the corner state 
+if optimizer == 1
+    options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1E-1);
+    resonance = fminbnd(@costfunction,0,1,options);
+end
 function cost = costfunction(X)
+% Define the filename of your PHOENIX release
+filename = 'phoenix_64.exe';
+% Define the directory path
+directoryPath_load = 'data/loads/';
+% Define the directory path
+directoryPath_result = 'data/results/';
 
 % time evolution settings
-tmax = 4000;              %time-evolution range
+tmax = 100;              %time-evolution range
 dt = 0.006;               %time-step
 
 % system discretisation
@@ -90,8 +153,6 @@ dxy = 0.1;                 %spatial discretization step
 L = N*dxy;                 %real space grid length
 x = sparse(dxy*(-N/2+1:N/2)).';   %x dimension discretization
 y = sparse(dxy*(-N/2+1:N/2)).';   %y dimension discretization
-kx = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
-ky = (2*pi*(-N/2+1:N/2)/L).';     %wavevector discretization
 
 % parameter definition
 hbar = 6.582119569E-1;  %hbar in eVps^-1
@@ -107,45 +168,43 @@ I_Noise = 1E-1; psi=I_Noise*(-1+2*rand(N,N)-1i+2*1i*rand(N,N));
 psi(1:N,1:N) = real(psi);
 psi(N+1:2*N,1:N) = imag(psi);
 header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) num2str(dxy)};
-writecell(header,'data/load/wavefunction_plus.txt','Delimiter',' ');
-writematrix(psi,'data/load/wavefunction_plus.txt','Delimiter',' ','WriteMode','append');
+writecell(header,[directoryPath_load,'wavefunction_plus.txt'],'Delimiter',' ');
+writematrix(psi,[directoryPath_load,'wavefunction_plus.txt'],'Delimiter',' ','WriteMode','append');
 
 potential(1:N,1:N) = real(pot*1e3);
 potential(N+1:2*N,1:N) = imag(pot);
-
-header = {'#' 'SIZE' num2str(N) num2str(N) num2str(L) num2str(L) num2str(dxy) num2str(dxy)};
-writecell(header,'data/load/potential_plus.txt','Delimiter',' ');
-writematrix(potential,'data/load/potential_plus.txt','Delimiter',' ','WriteMode','append');
+writecell(header,[directoryPath_load,'potential_plus.txt'],'Delimiter',' ');
+writematrix(potential,[directoryPath_load,'potential_plus.txt'],'Delimiter',' ','WriteMode','append');
 
 % pulse parameters
 omega = -5.68178730684262-X*1e-2;
 E0 = 5.6856E-3;
 
 %create runstring
-str1 = 'phoenix_64.exe ';                                                                                                  %call PULSE main.exe
+str1 = [filename, ' '];                                                                                                    %call main.exe
 str2 = ['--N ',num2str(N),' ',num2str(N),' --L ',num2str(L),' ',num2str(L),' --boundary zero zero '];                      %define the real-space grid
 str3 = ['--tmax ',num2str(tmax),' --tstep ',num2str(dt),' --outEvery 1 --fftEvery 1000 '];                                 %time discretization
 str4 = ['--pulse ',num2str(E0),' add 1E12 1E12 0 0 plus 10 0 gauss+noDivide time iexp 0 1E12 ',num2str(omega),' '];        %define resonant pump
-str5 = '--potential load data/load/potential_plus.txt 1 add plus --wavefunction load data/load/wavefunction_plus.txt 1 add plus ';              %load initial condition and potential
+str5 = ['--potential load ',directoryPath_load,'potential_plus.txt 1 add plus --wavefunction load ',directoryPath_load,'wavefunction_plus.txt 1 add plus '];      %load initial condition and potential
 str6 = ['--gammaC ',num2str(gammaC),' --gc ',num2str(gc),' --meff ',num2str(mc),' --hbarscaled ',num2str(hbar),' '];                            %set GP-Parameters
-str7 = '--path data/results/';                                                                                                                  %set output directory                                                                                     
-
+str7 = ['--path ',directoryPath_result];                                                                                                        %set output directory
+                                                                                  
 % execute PHOENIX
 inputstr = [str1,str2,str3,str4,str5,str6,str7];
-[~,cmdout] = system(inputstr,'CUDA_VISIBLE_DEVICES','0');
-htmlString = removeAnsiCodes(cmdout);
-disp(htmlString(end-2630:end));
+[~,cmdout] = system(inputstr);
+phoenix_output = removeAnsiCodes(cmdout);
+disp(phoenix_output);
 
 %post-processing: visualize results and evaluate cost-function
-psi = readmatrix('data/results/wavefunction_plus.txt');
+psi = readmatrix([directoryPath_result,'wavefunction_plus.txt']);
 psi=psi(1:N,1:N)+1i*psi(N+1:2*N,1:N);
 Y1 = abs(reshape(psi,N,N)).^2;
 
-pump = readmatrix('data/results/pulse_plus.txt');
+pump = readmatrix([directoryPath_result,'pulse_plus.txt']);
 pump=pump(1:N,1:N)+1i*pump(N+1:2*N,1:N);
 
-pot = readmatrix('data/results/potential_plus.txt');
-pot=pot(1:N,1:N)+1i*pot(N+1:2*N,1:N);
+pot = readmatrix([directoryPath_result,'potential_plus.txt']);
+pot=pot(1:N,1:N);
 
 argpsi=angle(reshape(psi,N,N));
 xrange = N/2:N-1;
@@ -162,7 +221,7 @@ pbaspect([1 1 1]);axis tight;set(gca,'YTickLabel',[]);xlabel('x (µm)');shading 
 cost = 1/max(max(Y1));
 end
 
-%----------------------------------String handeling functions----------------------------------%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function cleanedString = removeAnsiCodes(inputString)
     % Use regexprep to remove ANSI escape codes
     cleanedString = strrep(inputString, '[90m#[0m', ' ');
